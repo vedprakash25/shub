cea7c6b6541a84f48e3b9d0ee6f69d4371cef97c
ved1998karma <ved1998karma@gmail.com>
2022-01-28 18:34:45 +0530

    first commit practice in blacksoff

--- src/components/Pages/MagicScroll/index.js
+++ src/components/Pages/MagicScroll/index.js
@@ -22,13 +22,13 @@ const MagicScroll = () => {
             </Controller>
 
             <Controller>
-                <Scene duration={1000} pin>
+                <Scene offset={1000} >
                     <div className='sticky__example green'>Sticky Example</div>
                 </Scene>
-                <Scene duration={1000} pin>
+                <Scene offset={80} >
                     <div className='sticky__example pink'>Sticky Example</div>
                 </Scene>
-                <Scene duration={1000} pin>
+                <Scene offset={1000} >
                     <div className='sticky__example orange'>Sticky Example</div>
                 </Scene>
             </Controller>
